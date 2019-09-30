7d56a938d7e9ac44db5d07f7e85928e5ac25cf60
Aakash <aakash@zestard.net>
2019-09-30 12:41:32 +0530

    mobile menu and image section responsive changes - smit

--- src/scripts/snippets/mobile-menu.js
+++ src/scripts/snippets/mobile-menu.js
@@ -1,39 +1,39 @@
 theme.openMobileMenu = function() {
-  $("#mobile_drawer").addClass("active");
-  theme.blankBgOpen();
+    $("#mobile_drawer").addClass("active");
+    theme.blankBgOpen();
 };
 theme.closeMobileMenu = function() {
-  $("#mobile_drawer").removeClass("active");
-  theme.blankBgClose();
+    $("#mobile_drawer").removeClass("active");
+    theme.blankBgClose();
 };
 theme.blankBgOpen = function() {
-  $("html").addClass("overflow_hidden");
-  $("body").addClass("overflow_hidden");
+    $("html").addClass("overflow_hidden");
+    $("body").addClass("overflow_hidden");
 };
 theme.blankBgClose = function() {
-  $("html").removeClass("overflow_hidden");
-  $("body").removeClass("overflow_hidden");
+    $("html").removeClass("overflow_hidden");
+    $("body").removeClass("overflow_hidden");
 };
 $(document).ready(function() {
-  $("body").append($("#mobile_drawer"));
-  $("#icon-nav-mobile-menu-btn").click(function(e) {
-    e.stopPropagation();
-    theme.openMobileMenu();
-  });
-  $("#mobile_drawer .drawer_close").click(function() {
-    theme.closeMobileMenu();
-  });
-  $(document).on("click", "#mobile_drawer .li .icon_wrap", function(e) {
-    e.stopPropagation();
-    $("i", $(this)).toggleClass("icon-minus");
-    $(this).parent().next(".child_menu").slideToggle();
-  });
-  $(document).on("click", "#mobile_drawer .li a", function(e) {
-    e.stopPropagation();
-  });
-  $(document).on("click", "#mobile_drawer .child_li", function(e) {
-    e.stopPropagation();
-    var li = $(this);
-    $(".link_data", li).slideToggle();
-  }); 
+    $("body").append($("#mobile_drawer"));
+    $("#icon-nav-mobile-menu-btn").click(function(e) {
+        e.stopPropagation();
+        theme.openMobileMenu();
+    });
+    $("#mobile_drawer .drawer_close").click(function() {
+        theme.closeMobileMenu();
+    });
+    $(document).on("click", "#mobile_drawer .li .icon_wrap", function(e) {
+        e.stopPropagation();
+        $("i", $(this)).toggleClass("icon-minus");
+        $(this).parent().next(".child_menu").slideToggle();
+    });
+    $(document).on("click", "#mobile_drawer .li a", function(e) {
+        e.stopPropagation();
+    });
+    $(document).on("click", "#mobile_drawer .child_li", function(e) {
+        e.stopPropagation();
+        var li = $(this);
+        $(".link_data", li).slideToggle();
+    }); 
 });
