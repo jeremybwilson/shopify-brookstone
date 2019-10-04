05628a1d6b66f351476cb1b04f4583a867bc4b8c
Aakash <aakash@zestard.net>
2019-10-04 16:13:31 +0530

    spacing fix - smit

--- src/scripts/bol/other.js
+++ src/scripts/bol/other.js
@@ -1,53 +1,30 @@
 theme.clickEvent = function(){
-    $(".customer-navigation-title").click(function() {
-        $(this).parent().toggleClass("active");
-    });
+	$(".customer-navigation-title").click(function(){
+		$(this).parent().toggleClass("active");
+	});
 };
 
 theme.contactMessage = function(){
-    setTimeout(function(){
-        $(".successForm").hide();
-    }, 10000);
+	setTimeout(function(){
+		$(".successForm").hide();
+	}, 10000);
 };
 
-theme.relatedItems = function(){
-    $("#cart-related-collection").slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
-}
-
 theme.init = function(){
-    theme.clickEvent();
-    theme.contactMessage();
-    theme.relatedItems();
+	theme.clickEvent();
+	theme.contactMessage();
 };
 
 
 $(document).ready(function () {
-    (function() {
-        $(document).on('shopify:section:load', function(event) {
-            $('.no-fouc').removeClass('no-fouc');
-            $('.load-wait').addClass('hide');
-        });
-    }());
-    $('.no-fouc').removeClass('no-fouc');
-    $('.load-wait').addClass('hide');
+	(function () {
+		$(document).on('shopify:section:load', function (event) {
+			$('.no-fouc').removeClass('no-fouc');
+			$('.load-wait').addClass('hide');
+		});
+	}());
+	$('.no-fouc').removeClass('no-fouc');
+	$('.load-wait').addClass('hide'); 
 
-    theme.init();
+	theme.init();
 });
