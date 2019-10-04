a7c1d740741d819775f5dc208b9d3efd7e2b0842
Aakash <aakash@zestard.net>
2019-10-04 16:17:16 +0530

    spacing fix - smit

--- src/scripts/bol/other.js
+++ src/scripts/bol/other.js
@@ -10,9 +10,33 @@ theme.contactMessage = function(){
 	}, 10000);
 };
 
+
+theme.relatedItems = function() {
+    $("#cart-related-collection").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                slidesToShow: 1
+                }
+            }
+        ]
+    });
+}
+
 theme.init = function(){
 	theme.clickEvent();
 	theme.contactMessage();
+    theme.relatedItems();
 };
 
 
